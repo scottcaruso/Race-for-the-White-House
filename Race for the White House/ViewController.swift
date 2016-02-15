95db25b1f75ef027f733fdf043925e09adb14d9b
scottcaruso <scottcaruso@gmail.com>
2016-02-15 15:50:58 -0500

    Initial Commit

---
+++ Race for the White House/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Race for the White House
+//
+//  Created by Scott Caruso on 2/15/16.
+//  Copyright © 2016 Scott Caruso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
